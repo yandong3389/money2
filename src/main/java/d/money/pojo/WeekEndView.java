8c0935a6495833372133cff07728c7f0e6175f89
Lawliet <Lawliet@Lawliet-PC>
2015-03-07 21:43:06 +0800

    实体类

---
+++ src/main/java/d/money/pojo/WeekEndView.java
@@ -0,0 +1,16 @@
+package d.money.pojo;
+
+import d.money.pojo.base.WeekEnd;
+
+public class WeekEndView extends WeekEnd {
+
+	public String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
