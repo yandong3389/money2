4c7cbeffd1563e2cdd04cf19b9b0bbaeb37f1421
Lawliet <Lawliet@Lawliet-PC>
2015-03-08 20:58:02 +0800

    注册接点人验证，推荐人奖金计算BUG修复

--- src/main/java/d/money/service/MoneyCalculateService.java
+++ src/main/java/d/money/service/MoneyCalculateService.java
@@ -3,9 +3,11 @@ package d.money.service;
 import java.util.List;
 
 import d.money.common.utils.Node;
+import d.money.pojo.base.NodeExample;
 
 public interface MoneyCalculateService {
 	
+	public int countByExample(NodeExample example);
 	/**
 	 * 插入node数据
 	 * @param userId 当前新增加的用户ID
@@ -16,8 +18,9 @@ public interface MoneyCalculateService {
 	/**
 	 * 更新奖金数据,记录奖金获取历史
 	 * @param nodeId 本次追加节点ID
+	 * @param 介绍人ID(30%获取人)
 	 */
-	public void updateMoney (int nodeId);
+	public void updateMoney (int nodeId, int jsrId);
 	
 	/**
 	 * 获取旁系节点集合
